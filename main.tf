6db567a076da6bedba668bc6b679f03833ceeef9
Prakash Jha <jhaprakash6608@gmail.com>
2022-01-07 16:08:18 +0530

    ppp

--- main.tf
+++ main.tf
@@ -7,13 +7,13 @@ module "network" {
   }
 }
 
-# module "ec2" {
-#   source            = "./ec2"
-#   vpc_id            = module.network.vpc_id
-#   alb_subnet_ids    = module.network.public_subnet_ids
-#   team_name         = var.team_name
-#   private_subnet_id = module.network.private_subnet_ids[0]
-#   nat_gw_id         = module.network.nat_gateway_id
-#   region            = var.region
-# }
+module "ec2" {
+  source            = "./ec2"
+  vpc_id            = module.network.vpc_id
+  alb_subnet_ids    = module.network.public_subnet_ids
+  team_name         = var.team_name
+  private_subnet_id = module.network.private_subnet_ids[0]
+  nat_gw_id         = module.network.nat_gateway_id
+  region            = var.region
+}
 
